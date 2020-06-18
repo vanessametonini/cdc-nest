2adbf653d8797d710d8f7ba6f2e089266ea22465
Vanessa Me Tonini <vanessametonini@gmail.com>
2020-06-18 16:58:21 -0300

    Feito a rota para pegar 1 livro com dados completos

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,9 +1,8 @@
-import { Controller, Post, Body, Get, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseInterceptors, ClassSerializerInterceptor, Param, UsePipes, ValidationPipe } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BookEntity } from './book.entity';
 import { Repository } from 'typeorm';
 
-@UseInterceptors(ClassSerializerInterceptor)
 @Controller('book')
 export class BookController {
 
@@ -12,13 +11,28 @@ export class BookController {
   ){}
 
   @Post()
+  @UseInterceptors(ClassSerializerInterceptor)
   async create(@Body() book: BookEntity): Promise<BookEntity> {
     return await this.bookRepository.save(book)
   }
-
+  
   @Get()
+  @UseInterceptors(ClassSerializerInterceptor)
   async list() {
     return await this.bookRepository.find({ relations: ["category", "author"]})
+  }
+
+  @Get(':id')
+  async findOne(@Param() param): Promise<BookEntity> {
+
+    const id = param.id;
+
+    return await this.bookRepository.findOne({ 
+      where: { id },
+      relations: ["category", "author"] 
+    })
+
+
   }
   
 }
